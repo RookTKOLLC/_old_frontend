c56ef4ed4872d6d06a30f1f2543d760d4e37208f
RookTKO <olaf.a.minkowicz@gmail.com>
2022-01-30 21:55:22 -0800

    added progressbar

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from '@emotion/styled'
 import { css } from '@emotion/react'
 import { Link } from 'gatsby'
@@ -7,12 +7,30 @@ const Navbar = styled.div`
     background-color: #252423;
     position: sticky;
     width:100%;
-    box-shadow: 0px 10px 75px 0px rgba(54, 54, 54, 0.5);
+    box-shadow: 0 2px 4px rgb(0 0 0 / 50%);
     color: #ffffff;
 `
 const color = 'orange'
 export default function Header(){
-    const navItems = ['Propoganda', ] //this needs to be fixed for mobile //'Projects', 'Goodies', "About" 
+
+    const [scrollTop, setScrollTop] = useState(0);
+    const navItems = ['Propoganda','Projects', 'Goodies', 'About'  ] //this needs to be fixed for mobile //
+        
+    const onScroll = () => {
+        const docEl = document.documentElement;
+        const windowScrollHeight = docEl.scrollTop;
+        const currentWindowHeight = docEl.scrollHeight - docEl.clientHeight;
+
+        const scrolled = (windowScrollHeight / currentWindowHeight) * 100;
+
+        setScrollTop(scrolled)
+      };
+
+      useEffect(() => {
+          window.addEventListener("scroll", onScroll);
+
+          return () => window.removeEventListener('scroll', onScroll)
+      }, []);
         return (
             <Navbar>
                 <Link to="/" css={css`
@@ -79,6 +97,12 @@ export default function Header(){
                         `}> {value} </Link> 
                     </li>)}
                 </ul>
+                <div css={css`
+                    width: ${scrollTop}%;
+                    background-color: #ffa039;
+                    height: 10px;
+                `}>
+                </div>
             </Navbar>
         );
 }
